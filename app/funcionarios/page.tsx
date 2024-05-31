f479410f929593be2f80f14fd6bd3db71e002d8f
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-05-31 16:43:41 -0300

    .

--- app/funcionarios/page.tsx
+++ app/funcionarios/page.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import CardFuncionarios from '../components/funcionarios/CardFuncionarios'
+import { PrismaClient } from '@prisma/client'
 
-const Funcionarios = () => {
+export default async function Funcionarios (){
+  const prisma = new PrismaClient();
+
+  const funcionarios = await prisma.funcionario.findMany({
+    select: {id:true, nome: true, cargo: true, area: true}
+  });
   return (
     <div>
-      Funcionários
-      <CardFuncionarios
-      nomeFuncionario='Yan dos Santos Ramos'
-      cargoFuncionario='Programador'
-      areaFuncionario='Programação' />
+      Funcionarios
+      {funcionarios.map((f) => {
+        return(
+          <CardFuncionarios
+          key={f.id}
+          nomeFuncionario={f.nome}
+          cargoFuncionario={f.cargo}
+          areaFuncionario={f.area} />
+        )
+      })}
     </div>
   )
 }
-
-export default Funcionarios
