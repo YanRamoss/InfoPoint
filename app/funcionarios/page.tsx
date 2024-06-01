7de58ffcb22e05e8b84cb64539f17445ed4c7b14
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-05-31 21:47:18 -0300

    .

--- app/funcionarios/page.tsx
+++ app/funcionarios/page.tsx
@@ -2,7 +2,7 @@ import CardFuncionarios from '../components/funcionarios/CardFuncionarios'
 
 export default async function Funcionarios (){
 
-  const response = await fetch('https://infopoint-sigma.vercel.app/api/funcionario', {next: {revalidate: 10}, method: "GET"});
+  const response = await fetch('https://infopoint-sigma.vercel.app/api/funcionario/', {next: {revalidate: 10}, method: "GET"});
   const {data} = await response.json();
   console.log(data);
   return (
