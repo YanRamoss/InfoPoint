183b62dc5941e952f9771bd5ad4e8baa3e6e1997
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-06-03 17:40:56 -0300

    .

--- app/api/funcionario/route.ts
+++ app/api/funcionario/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/app/lib/prisma"
 
-export async function GET() {
+export async function GET(req: Request) {
 
     const data = await prisma.funcionario.findMany({
         select:{
