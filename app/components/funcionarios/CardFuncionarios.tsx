c9e7b978bca9c26fd0d1d1b5007d588edf263103
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-06-12 10:10:08 -0300

    .

--- app/components/funcionarios/CardFuncionarios.tsx
+++ app/components/funcionarios/CardFuncionarios.tsx
@@ -6,7 +6,7 @@ type propsType = {
 }
 
 const CardFuncionarios = (props:propsType) => {
-const CardFuncionarios = () => {
+// const CardFuncionarios = () => {
   return (
     <div className='flex flex-col w-[auto] h-[auto]'>
       <div className='flex w-[100%] h-[150px] bg-[#9b9b9b88] rounded-[3px]'>
