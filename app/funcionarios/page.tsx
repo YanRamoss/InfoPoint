9a89a34f59a9b9e7c907a3fa16d824d0cce2dd8f
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-06-12 10:43:38 -0300

    .

--- app/funcionarios/page.tsx
+++ app/funcionarios/page.tsx
@@ -7,7 +7,7 @@ export default async function Funcionarios (){
   console.log(data);
   return (
     <div className='flex w-[90%] h-auto flex-col mx-auto'>
-      <h2 className='text-center text-[25px] py-[20px]'>Funcionários</h2>
+      <h2 className='text-center text-[25px] py-[30px]'>Funcionários</h2>
       <div className="flex flex-row w-full h-auto gap-[50px] flex-wrap justify-center">
         {/* {data.map((f: any) => {
           return(
