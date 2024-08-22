09f10955d948e6837dc17b88d32ff7e546ff36e6
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-08-22 17:45:55 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1 className='text-center text-[25px] py-[30px]'>Quem Somos</h1>
-      {/* <Image src={HomeBanner} alt="Home" width={} height={100}/> */}
-      <div className="flex flex-row w-[90%] mx-auto gap-[30px] justify-center" >
+        <h1 className="flex items-center justify-center bg-[url('/images/pages/home-banner.jpg')] bg-cover bg-center h-[220px] text-[25px] py-[30px]">
+        Quem Somos
+        </h1>
+      <div className="flex flex-row w-[90%] mx-auto mt-10 gap-[30px] justify-center" >
         <div className="flex w-[80%] h-[500px] bg-[#b8b8b898] rounded-[3px]">
           .
         </div>
