27861bbc9350863ad81d85d6bc3593d203d64c5f
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-09-25 11:07:13 -0300

    Update page.tsx

--- app/calendario/page.tsx
+++ app/calendario/page.tsx
@@ -4,6 +4,10 @@ const Calendario = () => {
   return (
     <div>
       Calendário
+
+
+      <embed src="http://example.com/the.pdf" width="500" height="375" 
+       type="application/pdf">
     </div>
   )
 }
