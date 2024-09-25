1827338b1676e35784281a6f4c07fd2eabe1f0c8
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-09-25 11:11:49 -0300

    Update page.tsx

--- app/calendario/page.tsx
+++ app/calendario/page.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Calendario = () => {
   return (
+    <div className='flex w-[90%] h-auto flex-col mx-auto'>
       <h2 className='text-center text-[25px] py-[30px]'>Calendário</h2>
       <embed src="/calendario.pdf" width="500" height="375" 
        type="application/pdf">
+      </embed>
+    </div>
   )
 }
 
