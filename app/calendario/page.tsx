168b31e9e62de455a8fefb8d530c893c4009e3d2
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-09-25 11:15:23 -0300

    Update page.tsx

--- app/calendario/page.tsx
+++ app/calendario/page.tsx
@@ -4,7 +4,7 @@ const Calendario = () => {
   return (
     <div className='flex w-[90%] h-auto flex-col mx-auto'>
       <h2 className='text-center text-[25px] py-[30px]'>Calendário</h2>
-      <embed className='mx-auto' src="/calendario.pdf" width="600" height="800" 
+      <embed className='mx-auto' src="/calendario.pdf" width="1000" height="800" 
        type="application/pdf">
       </embed>
     </div>
