bf3493b2b9b030d4f28aca2aa32c109e5fd32264
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-09-30 16:33:01 -0300

    .

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex w-full h-[220px] bg-iblue justify-center items-center'>
+    <div className='flex w-full h-[100px] bg-iblue justify-center items-center'>
       <p className='text-white font-medium'>InfoPoint - All Rights Reserved</p>
     </div>
   )
