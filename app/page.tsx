74074e33f0323b6e634e72edcd1ab10a9ad95235
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-09-30 16:51:15 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // import HomeBanner from '@/public/home-banner.jpg'
-import Carousel from "../components/carrossel/Carousel";
+import Carousel from "./components/carrossel/Carousel";
 const images = [
   "./Yan.jpg",
   "./Ana.jpg",
