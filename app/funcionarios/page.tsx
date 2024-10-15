ec3bcd248f3def79d895fe6b20581f2ab9eadbce
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-15 15:43:30 -0300

    .

--- app/funcionarios/page.tsx
+++ app/funcionarios/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import CardFuncionarios from '../components/funcionarios/CardFuncionarios'
 
 export default async function Funcionarios (){
@@ -8,6 +9,9 @@ export default async function Funcionarios (){
   return (
     <div className='flex w-[90%] h-auto flex-col mx-auto'>
       <h2 className='text-center text-[25px] py-[30px]'>Funcionários</h2>
+      <h3>
+        <Link href='/funcionarios/new'>Novo</Link>
+      </h3>
       <div className="flex flex-row w-full h-auto gap-[50px] flex-wrap justify-center">
          {data.map((f: any) => { 
           return(
