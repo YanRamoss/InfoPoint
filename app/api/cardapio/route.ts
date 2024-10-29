a569b74ba738257c6db9f2ac00ad3a78748ffa5f
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 09:17:55 -0300

    .

--- app/api/cardapio/route.ts
+++ app/api/cardapio/route.ts
@@ -1,3 +1,16 @@
+import prisma from "@/app/lib/prisma"
+import { triggerAsyncId } from "async_hooks";
+
 export async function GET(req: Request) {
-    return Response.json({message: 'Rota - Cardápio', status: 200})
+
+    const data = await prisma.cardapio.findMany({
+        select:{
+            prato: true,
+            data: true,
+        }
+    }) 
+    if(!data) {
+        Response.json({message: 'Cardapio not found', status: 404});
+    }
+    return Response.json({message: 'Cardapio found', status: 200, data: data})
 } 
--- app/components/cardapio/CardapioComp.tsx
+++ app/components/cardapio/CardapioComp.tsx
@@ -17,14 +17,16 @@ const menu = {
 };
 
 export default function CardapioComp() {
+  const response = await fetch('https://infopoint-sigma.vercel.app/api/cardapio', {next: {revalidate: 10}, method: "GET"});
+  const {data} = await response.json();
   return (
     <div className="max-w-4xl mx-auto min-h-[600px]">
       <h1 className="text-center text-[25px] py-[30px]">Cardápio Semanal</h1>
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5">
-        {Object.entries(menu).map(([day, meals]) => (
-          <div key={day} className="bg-white p-5 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold">{day}</h2>
-            <p><strong>Almoço:</strong> {meals.lunch}</p>
+        {Object.entries(data).map(([prato, dia]) => (
+          <div key={dia} className="bg-white p-5 rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold">{dia}</h2>
+            <p><strong>Almoço:</strong> {prato}</p>
           </div>
         ))}
       </div>
