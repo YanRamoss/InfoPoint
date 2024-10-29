56723e6702e1b252cb43096141d3a7a31edca26e
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 10:46:15 -0300

    .

--- app/components/cardapio/CardapioComp.tsx
+++ app/components/cardapio/CardapioComp.tsx
@@ -8,14 +8,14 @@ export default async function CardapioComp() {
     <div className="max-w-4xl mx-auto min-h-[600px]">
       <h1 className="text-center text-[25px] py-[30px]">Cardápio Semanal</h1>
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5">
-        {/* {data.map((f: any) => {
+        {data.map((f: any) => {
           return(
           <div key={f.prato} className="bg-white p-5 rounded-lg shadow-md">
             <h2 className="text-xl font-semibold">{f.data}</h2>
             <p><strong>Almoço:</strong> {f.prato}</p>
           </div>
           )
-        }) } */}
+        }) }
       </div>
     </div>
   );
