16d5ade292b307da51d94e47712bd6c1e9c219f7
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 10:32:46 -0300

    .

--- app/components/cardapio/CardapioComp.tsx
+++ app/components/cardapio/CardapioComp.tsx
@@ -1,19 +1,19 @@
 export default async function CardapioComp() {
   type InfoItem = {
     prato: string;
-    data: string;
+    dia: string; // Adjusted to use `dia` based on your previous context
   };
 
   const response = await fetch('https://infopoint-sigma.vercel.app/api/cardapio', { next: { revalidate: 10 }, method: "GET" });
-  const data = await response.json();
+  const data: InfoItem[] = await response.json(); // Specify that data is an array of InfoItem
 
   return (
     <div className="max-w-4xl mx-auto min-h-[600px]">
       <h1 className="text-center text-[25px] py-[30px]">Cardápio Semanal</h1>
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5">
-        {data.map((item, index) => (
+        {data.map((item: InfoItem, index: number) => ( // Specify the type for item
           <div key={index} className="bg-white p-5 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold">{item.data}</h2>
+            <h2 className="text-xl font-semibold">{item.dia}</h2>
             <p><strong>Almoço:</strong> {item.prato}</p>
           </div>
         ))}
