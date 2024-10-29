d89db062c5f5c937e9ab484b3b581760cb198df9
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-10-29 08:25:43 -0300

    Update page.tsx

--- app/admin/funcionarios/page.tsx
+++ app/admin/funcionarios/page.tsx
@@ -10,7 +10,7 @@ export default function AddFuncionario() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  const response = await fetch('http://localhost:3242/api/funcionario/add', {
+  const response = await fetch('https://infopoint-sigma.vercel.app/api/funcionario/add', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
