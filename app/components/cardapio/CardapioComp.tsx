71c89bcea00ba646d8d9d95992eed2de13fcea72
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 10:25:34 -0300

    .

--- app/components/cardapio/CardapioComp.tsx
+++ app/components/cardapio/CardapioComp.tsx
@@ -1,20 +1,21 @@
 export default async function CardapioComp() {
+  type InfoItem = {
+    prato: string;
+    dia: string;
+  };
 
-  type info = {
-    prato: string,
-    dia: string,
-  }
+  const response = await fetch('https://infopoint-sigma.vercel.app/api/cardapio', { next: { revalidate: 10 }, method: "GET" });
+  const data = await response.json();
+  const info: InfoItem[] = data.info; // Assuming `info` is an array of objects
 
-  const response = await fetch('https://infopoint-sigma.vercel.app/api/cardapio', {next: {revalidate: 10}, method: "GET"});
-  const {info} = await response.json();
   return (
     <div className="max-w-4xl mx-auto min-h-[600px]">
       <h1 className="text-center text-[25px] py-[30px]">Cardápio Semanal</h1>
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5">
-        {Object.entries(info).map(([prato, dia]) => (
-          <div key={info.prato} className="bg-white p-5 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold">{info.dia}</h2>
-            <p><strong>Almoço:</strong> {info.prato}</p>
+        {info.map((item, index) => (
+          <div key={index} className="bg-white p-5 rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold">{item.dia}</h2>
+            <p><strong>Almoço:</strong> {item.prato}</p>
           </div>
         ))}
       </div>
