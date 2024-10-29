2e3be2c9db136133ae065c46c90d13ee47d545b2
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 10:07:48 -0300

    .

--- app/components/cardapio/CardapioComp.tsx
+++ app/components/cardapio/CardapioComp.tsx
@@ -5,9 +5,9 @@ export default async function CardapioComp() {
     <div className="max-w-4xl mx-auto min-h-[600px]">
       <h1 className="text-center text-[25px] py-[30px]">Cardápio Semanal</h1>
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5">
-        {Object.entries(info).map(([prato, data]) => (
+        {Object.entries(info).map(([prato, datap]) => (
           <div key={prato} className="bg-white p-5 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold">{data}</h2>
+            <h2 className="text-xl font-semibold">{datap}</h2>
             <p><strong>Almoço:</strong> {prato}</p>
           </div>
         ))}
