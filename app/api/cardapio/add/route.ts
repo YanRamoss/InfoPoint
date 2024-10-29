c4804652da69305f4f814e5ab8065401b1df8bd4
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-10-29 08:40:17 -0300

    Update route.ts

--- app/api/cardapio/add/route.ts
+++ app/api/cardapio/add/route.ts
@@ -2,13 +2,13 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 export async function GET(req: Request) {
-  const { prato, data } = await req.json();
+  const { prato, datap } = await req.json();
 
     try {
       const cardapio = await prisma.cardapio.create({
         data: {
           prato,
-          data
+          datap
         },
       });
       Response.json(cardapio);
