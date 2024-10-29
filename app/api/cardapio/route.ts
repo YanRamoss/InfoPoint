927d0f162ad5be356bbe3e63eb4fb84fcda688b0
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 11:35:29 -0300

    1.1

--- app/api/cardapio/route.ts
+++ app/api/cardapio/route.ts
@@ -2,7 +2,12 @@ import prisma from "@/app/lib/prisma"
 
 export async function GET(req: Request) {
 
-    const cardapio = await prisma.cardapio.deleteMany();
+    const cardapio = await prisma.cardapio.findMany({
+        select:{
+            prato: true,
+            data: true
+        }
+    }) 
     if(!cardapio) {
         Response.json({message: 'Cardapio not found', status: 404});
     }
