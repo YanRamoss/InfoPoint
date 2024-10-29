f14f5c982783d3fdadbe9dd7abb90b7b304e44c6
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-10-29 09:02:19 -0300

    Update route.ts

--- app/api/cardapio/add/route.ts
+++ app/api/cardapio/add/route.ts
@@ -1,20 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-export async function POST(req: Request) {
-  const { prato, data } = await req.json();
 
-    try {
-      const cardapio = await prisma.cardapio.create({
+export async function POST(req: Request) {
+  try {
+  const { prato, data } = await req.json();
+    const cardapio = await prisma.cardapio.create({
         data: {
           prato,
           data
         },
       });
    
-    return Response.json({message: funcionario, status: 201 });
+    return Response.json({message: cardapio, status: 201 });
   } catch (error) {
     console.error(error);
-    return Response.json({ error: 'Failed to add funcionario' }, { status: 500 });
+    return Response.json({ error: 'Failed to add cardapio' }, { status: 500 });
   }
 }
