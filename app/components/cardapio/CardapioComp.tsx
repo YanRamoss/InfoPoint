d3b886e1f3bca916a13ab0f570800cf30eff470b
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 11:50:19 -0300

    .

--- app/components/cardapio/CardapioComp.tsx
+++ app/components/cardapio/CardapioComp.tsx
@@ -2,8 +2,9 @@ export default async function CardapioComp() {
 
 
   const response = await fetch('https://infopoint-sigma.vercel.app/api/cardapio', { next: { revalidate: 10 }, method: "GET" });
-  const {data} = await response.json();
-  console.log(data);
+  // const {data} = await response.json();
+  const data = [{"prato": "Arroz e Frango", "dia": "Segunda-feira"},{"prato": "Arroz e Frango", "dia": "Terça-feira"}];
+
   return (
     <div className="max-w-4xl mx-auto min-h-[600px]">
       <h1 className="text-center text-[25px] py-[30px]">Cardápio Semanal</h1>
