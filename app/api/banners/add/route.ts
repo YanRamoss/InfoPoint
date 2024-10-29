069851f21288a50c49fec91e384f592281c2492c
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-10-29 09:03:00 -0300

    Update route.ts

--- app/api/banners/add/route.ts
+++ app/api/banners/add/route.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-export async function POST(req: Request) {
-  const { nome, imagem } = await req.json();
 
-    try {
+export async function POST(req: Request) {
+  try {
+  const { nome, imagem } = await req.json();
       const banners = await prisma.banners.create({
         data: {
           nome,
