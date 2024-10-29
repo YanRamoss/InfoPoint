507a561663925dcf8491affb1b699684111a6655
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-29 11:09:29 -0300

    .

--- app/api/cardapio/add/route.ts
+++ app/api/cardapio/add/route.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 export async function POST(req: Request) {
   try {
   const deletedCardapio = await prisma.cardapio.deleteMany();
-    return Response.json({message: cardapio, status: 201 });
+  return;
   } catch (error) {
     console.error(error);
     return Response.json({ error: 'Failed to add cardapio' }, { status: 500 });
