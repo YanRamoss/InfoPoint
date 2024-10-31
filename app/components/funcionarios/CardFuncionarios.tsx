0a88f0a727634149cdd236c1f5d6c70eadcb4299
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-10-31 16:37:18 -0300

    .

--- app/components/funcionarios/CardFuncionarios.tsx
+++ app/components/funcionarios/CardFuncionarios.tsx
@@ -1,28 +1,59 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
-type propsType = {
-    nomeFuncionario:string,
-    cargoFuncionario:string,
-    areaFuncionario?:string,
-    fotoFuncionario:string,
+import React, { useEffect, useState } from 'react'
+type Funcionario = {
+    nome:string,
+    cargo:string,
+    area?:string,
+    foto:string,
 }
 
-const CardFuncionarios = (props:propsType) => {
+const CardFuncionarios = () => {
+  const [data, setData] = useState<Funcionario[]>([]);
+  const [error, setError] = useState();
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://infopoint-sigma.vercel.app/api/funcionario');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const result = await response.json();
+      setData(result.data);
+    } catch (err:any) {
+      setError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Fetch data on mount
+
+    const interval = setInterval(() => {
+      fetchData(); // Re-fetch data every 10 seconds
+    }, 10000); // 10000 ms = 10 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []); // Empty dependency array means this effect runs once on mount
+
   return (
+    <>
+    
+      {data.map((f, index) => (
     <div className='flex flex-col w-[180px] h-[auto]'>
-      <div className="flex relative w-[80%] h-[150px] rounded-[3px] cursor-pointer">  
+        <div className="flex relative w-[80%] h-[150px] rounded-[3px] cursor-pointer">  
         <Image
-        src={props.fotoFuncionario}
-        alt={props.nomeFuncionario}
+        src={'/'}
+        alt={f.nome}
         fill={true}
         quality={100}
         style={{objectFit: "cover"}}
         />
-      </div>
-      <p>{props.nomeFuncionario}</p>
-      <p>{props.cargoFuncionario}</p>
-      <p>Área de Atuação</p>
-      </div>
+        </div>
+        <p>{f.nome}</p>
+        <p>{f.cargo}</p>
+        <p>Área de Atuação</p>
+        </div>
+      ))}</>
   )
 }
 
--- app/funcionarios/page.tsx
+++ app/funcionarios/page.tsx
@@ -2,26 +2,13 @@ import Link from 'next/link';
 import CardFuncionarios from '../components/funcionarios/CardFuncionarios'
 
 export default async function Funcionarios (){
-  const PORT = process.env.PORT;
-
-  const response = await fetch('https://infopoint-sigma.vercel.app/api/funcionario', {next: {revalidate: 10}, method: "GET"});
-  const {data} = await response.json();
-  console.log(data);
+  
   return (
     <div className='flex w-[90%] h-auto flex-col mx-auto'>
       <h2 className='text-center text-[25px] py-[30px]'>Funcionários</h2>
       <div className="flex flex-row w-full h-auto gap-[50px] flex-wrap justify-center">
-         {data.map((f: any) => { 
-          return(
-            <CardFuncionarios
-            key={f.nome}
-            nomeFuncionario={f.nome}
-            cargoFuncionario={f.cargo}
-            areaFuncionario={f.area}
-            fotoFuncionario={f.imagem} />
-          )
-        })} 
-      </div>
+        <CardFuncionarios />
+        </div>
     </div>
   )
 }
