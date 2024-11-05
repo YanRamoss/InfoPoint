5a75eff2fc473e5d3f8a915ce1f7ae91fa8fa51e
Yan Ramos <100092216+YanRamoss@users.noreply.github.com>
2024-11-05 08:20:59 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,18 @@
+
+import Link from 'next/link'
+    
 export default function Login() {
     return(
         <>
         <h1 className="text-center font-medium my-[15px]">Acesse sua conta</h1>
-        <form action="/admin" method="post" className="flex flex-col mx-auto w-[300px]">
+        <form className="flex flex-col mx-auto w-[300px]">
             <label htmlFor="usuario">Usuário</label>
             <input type="text" name="usuario" id="" placeholder="Usuário" className="outline outline-1 py-[5px] px-[5px] outline-gray rounded-[1px] mb-[15px]" />
 
             <label htmlFor="senha">Senha</label>
             <input type="password" name="senha" placeholder="Senha" className="outline outline-1 py-[5px] px-[5px] outline-gray rounded-[1px] mb-[15px]"/>
 
-            <input type="submit" value="Login" className="bg-iblue cursor-pointer w-[100px] rounded-[3px] text-white text-sm mx-auto" />
+            <Link href="/login"></Link><input type="submit" value="Login" className="bg-iblue cursor-pointer w-[100px] rounded-[3px] text-white text-sm mx-auto" /></Link>
         </form>
         </>
     )
