b02515accac1f33bd9622027500b1f8140013bfe
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-11-25 08:03:03 -0300

    .

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -5,7 +5,7 @@ import InfoPoint from '@/public/Infopoint.png';
 import HeaderTitle from './HeaderTitle';
 const Header = () => {
   return (
-    <header className='fixed'>
+    <header>
         <div className='flex flex-row h-[70px] w-screen items-center justify-center border-b-4 border-[#43FEF6] bg-iblue'>
             <Link href='./'>
               <Image
