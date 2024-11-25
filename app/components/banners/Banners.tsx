d5c24db07741296782e9df1cffefccf495a5b67e
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-11-25 14:58:12 -0300

    .

--- app/components/banners/Banners.tsx
+++ app/components/banners/Banners.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 export default function Banners() {
-export default function Home() {
     const [currentImage, setCurrentImage] = useState(0);
 
     // Função para avançar para a próxima imagem
