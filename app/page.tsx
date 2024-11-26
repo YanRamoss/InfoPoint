15eb0157bc7a10111a902bff90f6a7644522b01a
YanRamoss <yan.ramos@etec.sp.gov.br>
2024-11-25 22:16:39 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home(){
         </div>
         <div className="flex flex-col sm:w-[100%] md:w-[80%] text-[20px] gap-[20px] bg-iblue p-[20px] text-white">
           <p className='text-justify'>
-            Nós somos alunos da ETEC Rio Grande da Serra do 3° ano de Desenvolvimento de Sistemas e estamos desenvolvendo uma empresa durante as aulas de TCC (trabalho de conclusão de curso).
+            Nós somos alunos da ETEC Rio Grande da Serra do 3° ano de Desenvolvimento de Sistemas e desenvolvemos essa empresa durante as aulas de TCC (trabalho de conclusão de curso).
           </p>
           <p className='text-justify'>
             Nosso projeto inclui um totem de informações interativo para otimizar a divulgação de informações sobre a unidade escolar, proporcionando uma experiência de comunicação eficiente e acessível para alunos, pais, professores e comunidade, promovendo assim o engajamento e o fortalecimento dos laços entre a escola e seu público-alvo. 
@@ -22,21 +22,20 @@ export default function Home(){
       <div className="flex flex-col w-[90%] mt-[80px] mb-[30px] mx-auto gap-[30px] text-black">
         <div className="flex flex-col">
           <h2 className='text-[23px] font-semibold'>Missão</h2>
-          <p className='text-[17px]'>Nossa missão é transformar a rotina e vida escolar dos alunos e docentes, tornando-a mais prática e simples em questão da transmissão de informções, fazendo com que todo frequentador do âmbito escolar tenha melhor experiência acadêmica.</p>
+          <p className='text-[17px] text-justify'>Nossa missão é transformar a rotina e vida escolar dos alunos e docentes, tornando-a mais pratica e simples em questão da transmissão de informações, fazendo com que todo frequentador do âmbito escolar tenha melhor experiência acadêmica.</p>
         </div>
         <div className="flex flex-col">
           <h2 className='text-[23px] font-semibold'>Visão</h2>
-          <p className='text-[17px]'>Unir a comunidade e mudar a forma padrão de transmitir a informação escolar através da tecnologia, promovendo um espaço onde a comunicação seja transparente, ágil e acessível para todos, além da expansão do projeto a longo prazo.</p>
+          <p className='text-[17px] text-justify'>Unir a comunidade e mudar a forma padrão de transmitir a informação escolar através da tecnologia, promovendo um espaço onde a comunicação seja transparente, ágil e acessível para todos, além da expansão do projeto a longo prazo.</p>
         </div>
         <div className="flex flex-col">
           <h2 className='text-[23px] font-semibold'>Valores</h2>
           <ul className='text-[17px]'>
-          <li><b>Transparência:</b> Trabalhamos de forma honesta, mostrando todos nossos objetivos e sendo fiel ao nosso público;</li>
-          <li><b>Acessibilidade:</b> é um sistema acessível, pois está localizado dentro da escola, disponível para os que frequentam esse ambiente;</li>
-          <li><b>Ética:</b> trabalhamos de forma ética, guardando todas as informações passadas a nós;</li>
-          <li><b>Responsabilidade social:</b> Como é um projeto que envolve público, foi tudo pensado para que seja uma experiência única para cada um;</li>
-          <li>Simplicidade chamativa e inovadora para que todos consigam utilizar o totem da melhor forma, tendo uma boa experiência;</li>
-        </ul>
+          <li className='text-justify'><b>Transparência:</b> Trabalhamos de forma honesta, mostrando todos nossos objetivos e sendo fiel ao nosso público;</li>
+          <li className='text-justify'><b>Acessibilidade:</b> é um sistema acessível, pois está localizado dentro da escola, disponível para os que frequentam esse ambiente;</li>
+          <li className='text-justify'><b>Ética:</b> trabalhamos de forma ética, guardando todas as informações passadas a nós;</li>
+          <li className='text-justify'><b>Responsabilidade social:</b> como é um projeto que envolve público, foi tudo pensado para que seja uma experiência única para cada um;</li>
+      </ul>
         </div>
         <a href="https://www.meupesominhajornada.com.br/hcp-locator1.html?cid=paid3-qikb2gjzvy" className='flex mx-auto items-center justify-center'>
         <img src="./imagens/anuncio.gif" alt="" />
